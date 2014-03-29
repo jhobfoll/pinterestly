8e716f7040fcb51a699e1b57bc472ed0f4815921
Jordan Hobfoll <jhobfoll@gmail.com>
2014-03-29 17:56:06 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinterestly::Application.config.secret_key_base = '0c6f2b9a60b430082b34a21757a7aa1a44cb624a6217ebc5605dc7346fc5249bb16f91cfeb945f3ce01de320897c5d9645e4935e04b1f198fb6c9f4ca08e6f71'
