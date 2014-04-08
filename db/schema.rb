4ccff7e133fd2eee861730ecd6b22d7c502f91a0
Jordan Hobfoll <jhobfoll@gmail.com>
2014-04-08 00:32:08 -0500

    Integrated devise and creaed pins scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140330205945) do
+ActiveRecord::Schema.define(version: 20140407215448) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
